fbb2152dd3d301955186efcc443034c265eeeb8c
mxk5025 <michaelkha213@gmail.com>
2019-01-04 23:38:35 -0500

    Added a Round class to organize who won a series of turns

---
+++ src/main/java/com/thirteen/model/Round.java
@@ -0,0 +1,12 @@
+package com.thirteen.model;
+
+/**
+ * TODO: implement class to hold turns
+ * Represents a round, a state where the first turn can make any legal play.
+ *
+ * @author Michael Kha
+ */
+public class Round {
+
+
+}
