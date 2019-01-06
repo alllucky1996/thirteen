ef5c7509c6c66ac68e2491f76f89ce6fa20a4add
mxk5025 <michaelkha213@gmail.com>
2019-01-06 12:09:55 -0500

    Implemented Round class

--- src/main/java/com/thirteen/model/Round.java
+++ src/main/java/com/thirteen/model/Round.java
@@ -1,12 +1,46 @@
 package com.thirteen.model;
 
+import java.util.List;
+
 /**
- * TODO: implement class to hold turns
- * Represents a round, a state where the first turn can make any legal play.
+ * Represents a round, consisting of a number of turns.
+ * A round is a list of turns created upon the first turn of the
+ * round being made. The first turn dictates the type of successive plays
+ * that are possible.
  *
  * @author Michael Kha
  */
 public class Round {
 
+    /**
+     * Turns that occurred within the round
+     */
+    private List<Turn> turns;
 
+    /**
+     * Create a round upon the first turn being made.
+     * @param turns Any empty list
+     * @param turn  Starting turn for the round
+     */
+    public Round(List<Turn> turns, Turn turn) {
+        this.turns = turns;
+        turns.add(turn);
+    }
+
+    /**
+     * Get all the turns made this round.
+     * @return  All the turns made
+     */
+    public List<Turn> getTurns() {
+        return turns;
+    }
+
+    /**
+     * Get the last turn made this round,
+     * preconditions: turns is not empty
+     * @return  The last turn made
+     */
+    public Turn getLastTurn() {
+        return turns.get(turns.size()-1);
+    }
 }
