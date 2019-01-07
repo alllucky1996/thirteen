176cee75ae41738aa42293abb0cb0ab104540b9f
mxk5025 <michaelkha213@gmail.com>
2019-01-06 21:18:22 -0500

    Added method to the CardStructure interface

--- src/main/java/com/thirteen/model/CardStructure.java
+++ src/main/java/com/thirteen/model/CardStructure.java
@@ -28,4 +28,10 @@ public interface CardStructure {
      */
     boolean isEmpty();
 
+    /**
+     * Appends all of the cards in the specified list to the end of this list.
+     * @param cards  List containing elements to be added to this list
+     */
+    void addAll(List<Card> cards);
+
 }
