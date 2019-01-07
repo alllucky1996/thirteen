8ce139ece9705006b50bc8bd613a0b4732c7496e
mxk5025 <michaelkha213@gmail.com>
2019-01-07 01:37:07 -0500

    Added Application class

---
+++ src/main/java/com/thirteen/view/GameTable.java
@@ -0,0 +1,19 @@
+package com.thirteen.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Michael Kha
+ */
+public class GameTable extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        // Take in player name
+        Parameters parameters = getParameters();
+
+    }
+
+}
