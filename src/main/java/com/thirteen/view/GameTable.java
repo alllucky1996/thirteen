838db7f83352fbe6eae225144ff7219d34e71f75
mxk5025 <michaelkha213@gmail.com>
2019-01-13 01:07:22 -0500

    Revert "Updated README"
    This reverts commit a82ebdc38921a4dfa49c121d9be9295599afeb75.

--- src/main/java/com/thirteen/view/GameTable.java
+++ src/main/java/com/thirteen/view/GameTable.java
@@ -1,10 +1,6 @@
 package com.thirteen.view;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -13,31 +9,11 @@ import javafx.stage.Stage;
  */
 public class GameTable extends Application {
 
-    /**
-     * Title of the stage
-     */
-    private static final String TITLE = "Thirteen";
-
     @Override
     public void start(Stage stage) {
         // Take in player name
         Parameters parameters = getParameters();
 
-        // Build scene
-        GridPane gridPane = new GridPane();
-        Image image = new Image("");
-        for (int col = 0; col < 3; col++) {
-            for (int row = 0; row < 3; row++) {
-                gridPane.add(new ImageView(image), col, row);
-            }
-        }
-
-        Scene scene = new Scene(gridPane);
-
-        // Setup stage
-        stage.setTitle(TITLE);
-        stage.setScene(scene);
-        stage.show();
     }
 
 }
