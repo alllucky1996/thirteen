a82ebdc38921a4dfa49c121d9be9295599afeb75
mxk5025 <michaelkha213@gmail.com>
2019-01-13 01:04:35 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+## thirteen
+
 Thirteen is a version of the Vietnamese card game, Tiến Lên.
+
+All rules in this version are based on personal experience and online references.
 
 The first iteration of the game will be a JavaFX application and eventually
 will turn into a web application.
--- src/main/java/com/thirteen/view/GameTable.java
+++ src/main/java/com/thirteen/view/GameTable.java
@@ -1,6 +1,10 @@
 package com.thirteen.view;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -9,11 +13,31 @@ import javafx.stage.Stage;
  */
 public class GameTable extends Application {
 
+    /**
+     * Title of the stage
+     */
+    private static final String TITLE = "Thirteen";
+
     @Override
     public void start(Stage stage) {
         // Take in player name
         Parameters parameters = getParameters();
 
+        // Build scene
+        GridPane gridPane = new GridPane();
+        Image image = new Image("");
+        for (int col = 0; col < 3; col++) {
+            for (int row = 0; row < 3; row++) {
+                gridPane.add(new ImageView(image), col, row);
+            }
+        }
+
+        Scene scene = new Scene(gridPane);
+
+        // Setup stage
+        stage.setTitle(TITLE);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
