b8480a03cf227bfc908b8a4a72523c9e2f0a4f8e
mxk5025 <michaelkha213@gmail.com>
2019-01-18 23:45:19 -0500

    Stubbed player pane design

---
+++ src/main/java/com/thirteen/view/PlayerPane.java
@@ -0,0 +1,25 @@
+package com.thirteen.view;
+
+import javafx.scene.layout.GridPane;
+
+public class PlayerPane extends GridPane implements ThirteenView {
+
+    PlayerPane() {
+
+
+        // make move button at bottom
+
+        // show text indicating if move is valid
+
+        // undo move button
+
+        // end turn button
+
+        // if move is empty: display pass move warning through an alert
+        // alert asks "Are you sure?" in the prompt and in the body
+        // "You are about to submit a passing move."
+        // while giving two options, "Cancel" or "Submit turn"
+
+    }
+
+}
