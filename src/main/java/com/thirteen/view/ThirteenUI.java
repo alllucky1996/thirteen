bc780c8565fcd29d39e594c7ed3dd864611c58ef
mxk5025 <michaelkha213@gmail.com>
2019-01-25 15:50:43 -0500

    Added ThirteenUI to run application

---
+++ src/main/java/com/thirteen/view/ThirteenUI.java
@@ -0,0 +1,61 @@
+package com.thirteen.view;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The application representing the graphical user interface
+ * for the game of thirteen.
+ *
+ * @author Michael Kha
+ */
+public class ThirteenUI extends Application {
+
+    /**
+     * Title of the stage
+     */
+    private static final String TITLE = "Thirteen";
+
+    private static final int MAX_WIDTH = 1280;
+
+    private static final int MAX_HEIGHT = 720;
+
+    /**
+     * Create the content for the scene.
+     * @return  Parent node populated with content
+     */
+    private Parent createContent() {
+        return new GamePane();
+    }
+
+    /**
+     * Initialize the application by creating and loading content onto
+     * the stage to be shown.
+     * @param stage Stage to setup and show
+     */
+    @Override
+    public void start(Stage stage) {
+        // Take in player name
+        Parameters parameters = getParameters();
+
+        // Build scene
+        Parent parent = createContent();
+        Scene scene = new Scene(parent);
+
+        // Setup stage
+        stage.setTitle(TITLE);
+        stage.setScene(scene);
+        // Set size of application window to only be a single size
+        stage.setMaxWidth(MAX_WIDTH);
+        stage.setMaxHeight(MAX_HEIGHT);
+        stage.setMaximized(true);
+        stage.setMinHeight(MAX_HEIGHT);
+        stage.setMinWidth(MAX_WIDTH);
+        // Disable mouse to show resize arrows
+        stage.setResizable(false);
+        stage.show();
+    }
+
+}
