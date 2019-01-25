bf27cd3fdd2376f2632c2e35535231f75214277c
mxk5025 <michaelkha213@gmail.com>
2019-01-25 15:44:33 -0500

    Changed sizes and more responsive card visuals

--- src/main/java/com/thirteen/view/CardButton.java
+++ src/main/java/com/thirteen/view/CardButton.java
@@ -7,13 +7,17 @@ import javafx.scene.input.MouseEvent;
 
 public class CardButton extends ToggleButton implements CardView {
 
-    private static final double HOVER_WIDTH = CARD_WIDTH /5;
+    private static final double HOVER_WIDTH = CARD_WIDTH / 6;
 
-    private static final double HOVER_HEIGHT = CARD_HEIGHT /5;
+    private static final double HOVER_HEIGHT = CARD_HEIGHT / 6;
 
-    private static final double DEFAULT_WIDTH = CARD_WIDTH /6;
+    private static final double SELECT_WIDTH = CARD_WIDTH / 8.5;
 
-    private static final double DEFAULT_HEIGHT = CARD_HEIGHT /6;
+    private static final double SELECT_HEIGHT = CARD_HEIGHT / 8.5;
+
+    private static final double DEFAULT_WIDTH = CARD_WIDTH / 10;
+
+    private static final double DEFAULT_HEIGHT = CARD_HEIGHT / 10;
 
     private CardImage cardImage;
 
@@ -34,6 +38,18 @@ public class CardButton extends ToggleButton implements CardView {
             public void handle(MouseEvent event) {
                 imageView.setFitWidth(HOVER_WIDTH);
                 imageView.setFitHeight(HOVER_HEIGHT);
+            }
+        });
+        setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (isSelected()) {
+                    imageView.setFitWidth(SELECT_WIDTH);
+                    imageView.setFitHeight(SELECT_HEIGHT);
+                } else {
+                    imageView.setFitWidth(DEFAULT_WIDTH);
+                    imageView.setFitHeight(DEFAULT_HEIGHT);
+                }
             }
         });
         setOnMouseExited(new EventHandler<MouseEvent>() {
