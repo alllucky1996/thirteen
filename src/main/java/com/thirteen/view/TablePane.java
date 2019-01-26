c91d966c7cd959fe2d840522394d41483924ffbc
mxk5025 <michaelkha213@gmail.com>
2019-01-25 22:16:20 -0500

    Changing position of text

--- src/main/java/com/thirteen/view/TablePane.java
+++ src/main/java/com/thirteen/view/TablePane.java
@@ -2,13 +2,19 @@ package com.thirteen.view;
 
 import com.thirteen.model.Hand;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Shape;
 
 /**
  * TODO: document
  */
-public class TablePane extends Pane {
+public class TablePane extends Pane implements ThirteenView {
 
     TablePane(Hand... hands) {
+        Shape ellipse = new Ellipse(600, 350, 300, 150);
+        ellipse.setFill(TABLE);
+        getChildren().add(ellipse);
+
         OpposingHandPane[] handPanes = new OpposingHandPane[hands.length];
         for (int i = 0; i < hands.length; i++) {
             handPanes[i] = new OpposingHandPane(hands[i]);
@@ -18,20 +24,21 @@ public class TablePane extends Pane {
             getChildren().add(handPanes[count]);
             switch (count) {
                 case 0:
-                    handPanes[count].setLayoutX(400);
-                    handPanes[count].setLayoutY(50);
+                    handPanes[count].setLayoutX(500);
+                    handPanes[count].setLayoutY(150);
                     break;
                 case 1:
                     handPanes[count].setLayoutX(100);
-                    handPanes[count].setLayoutY(150);
+                    handPanes[count].setLayoutY(300);
                     break;
                 case 2:
-                    handPanes[count].setLayoutX(700);
-                    handPanes[count].setLayoutY(150);
+                    handPanes[count].setLayoutX(900);
+                    handPanes[count].setLayoutY(300);
                     break;
             }
             count++;
         }
+        setPadding(DEFAULT_INSETS);
     }
 
 }
