a14ce69277f86dc1a623fd818d77db382e966b61
mxk5025 <michaelkha213@gmail.com>
2019-01-30 21:01:04 -0500

    Documented

--- src/main/java/com/thirteen/view/ThirteenView.java
+++ src/main/java/com/thirteen/view/ThirteenView.java
@@ -3,29 +3,38 @@ package com.thirteen.view;
 import javafx.geometry.Insets;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 
 /**
- * TODO: document
+ * Assets and constants to be used by game UI.
+ *
+ * @author Michael Kha
  */
 public interface ThirteenView {
 
+    /**
+     * Default insets value
+     */
     Insets DEFAULT_INSETS = new Insets(15);
 
+    /**
+     * Path for image files
+     */
     String IMAGE_LOCATION = "file:src/main/resources/img/";
 
+    /**
+     * The game table pattern
+     */
     ImagePattern TABLE = new ImagePattern(new Image(IMAGE_LOCATION + "table-texture.jpg"));
 
-    Background TABLE_BACKGROUND =
-            new Background(new BackgroundImage(
-            new Image(IMAGE_LOCATION + "table-texture.jpg"), BackgroundRepeat.NO_REPEAT,
-            BackgroundRepeat.NO_REPEAT, null, null));
-
-    Background LIGHT_GREEN_BACKGROUND =
-            new Background(new BackgroundFill(Color.GREEN.brighter(),
-                    CornerRadii.EMPTY, Insets.EMPTY));
-
+    /**
+     * Generic method to be used by any region to resize itself to
+     * a default size characterized by its min and max widths and heights
+     * being equal.
+     * @param region    Region to set to default size
+     * @param width     Default width
+     * @param height    Default height
+     */
     static void setDefSize(Region region, double width, double height) {
         region.setMinSize(width, height);
         region.setMaxSize(width, height);
