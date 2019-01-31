53f11e92afd910b78c8ec8ac1c88e35de5763ce8
mxk5025 <michaelkha213@gmail.com>
2019-01-30 21:08:07 -0500

    Updated and documented

--- src/main/java/com/thirteen/view/OpposingHandPane.java
+++ src/main/java/com/thirteen/view/OpposingHandPane.java
@@ -10,24 +10,39 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 /**
- * TODO: document
+ * Pane which holds the opposing players' hands as unknown cards.
+ *
+ * @author Michael Kha
  */
 public class OpposingHandPane extends Pane implements CardView, ThirteenView {
 
+    /**
+     * Default width using the card width
+     */
     private static final double DEFAULT_WIDTH = CARD_WIDTH / 14;
-
+    /**
+     * Default height using the card height
+     */
     private static final double DEFAULT_HEIGHT = CARD_HEIGHT / 14;
-
+    /**
+     * Default relative x position used to create visual staggering card effect
+     */
     private static final double DEFAULT_X_POS = DEFAULT_WIDTH / 2.5;
 
+    /**
+     * Text to display the number of cards
+     */
     private Text cardNumInfo;
 
-    // TODO: change to hand size parameter?
+    /**
+     * Create an opposing hand pane given a known hand. A hand is needed
+     * to correctly update the number of cards in hand.
+     * @param hand  Hand to represent
+     */
     OpposingHandPane(Hand hand) {
         ObservableList<Node> children = getChildren();
         CardID cardBack = new CardID(Rank.BACK, Suit.BACK);
         cardNumInfo = new Text();
-        cardNumInfo.setText(hand.size() + " CARDS");
         cardNumInfo.setLayoutY(-5);
         cardNumInfo.setVisible(false);
         children.add(cardNumInfo);
@@ -36,14 +51,19 @@ public class OpposingHandPane extends Pane implements CardView, ThirteenView {
             imageView.setLayoutX(DEFAULT_X_POS * i);
             children.add(imageView);
         }
-        setMouseEvents();
+        setMouseEvents(hand);
         setPadding(DEFAULT_INSETS);
     }
 
-    private void setMouseEvents() {
+    /**
+     * Set the mouse events for the pane. Upon hovering,
+     * additional info about the number of cards in hand is detailed.
+     */
+    private void setMouseEvents(Hand hand) {
         setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                cardNumInfo.setText(hand.size() + " CARDS");
                 cardNumInfo.setVisible(true);
             }
         });
@@ -55,8 +75,14 @@ public class OpposingHandPane extends Pane implements CardView, ThirteenView {
         });
     }
 
+    /**
+     * Create an image view and sets the fit width and fit height to default.
+     * @param cardID    The cardID to get the card image
+     * @return  An image view of the card
+     */
     private ImageView createImageView(CardID cardID) {
-        ImageView imageView = CardView.createImageView(CardImage.CARD_IMAGES.get(cardID));
+        ImageView imageView = CardView.createImageView(
+                CardImage.CARD_IMAGES.get(cardID));
         imageView.setFitWidth(DEFAULT_WIDTH);
         imageView.setFitHeight(DEFAULT_HEIGHT);
         return imageView;
