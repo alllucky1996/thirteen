25fc81a45b4bcc790b1633fbb1f3cc7d1e73c9bb
mxk5025 <michaelkha213@gmail.com>
2019-01-30 20:52:17 -0500

    Updated and documented

--- src/main/java/com/thirteen/view/HandPane.java
+++ src/main/java/com/thirteen/view/HandPane.java
@@ -6,22 +6,54 @@ import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * TODO: document
+ * An HBox holding instances of card buttons to be clicked on.
+ *
+ * @author Michael Kha
  */
 public class HandPane extends HBox implements ThirteenView {
 
-    private static final double MAX_WIDTH = 700;
+    /**
+     * Holds the cards and their visual representation as buttons
+     */
+    private Map<Card, CardButton> cards;
 
-    private static final double MAX_HEIGHT = 400;
-
+    /**
+     * Create a hand pane consisting of the cards in hand as
+     * selectable buttons.
+     * @param hand  The hand to use
+     */
     HandPane(Hand hand) {
+        cards = new HashMap<>();
         ObservableList<Node> children = getChildren();
         for (Card card : hand.getCards()) {
             // FIXME: Java 11 runtime exception :( using Java 9 for now
-            children.add(new CardButton(CardImage.CARD_IMAGES.get(card.getID())));
+            CardButton cardButton = new CardButton(CardImage.CARD_IMAGES.get(card.getID()));
+            cards.put(card, cardButton);
+            children.add(cardButton);
         }
         setPadding(DEFAULT_INSETS);
+    }
+
+    /**
+     * Removes all card buttons in hand that are toggled.
+     * @return  List of card objects that were removed
+     */
+    public List<Card> removeToggled() {
+        List<Card> removedCards = new ArrayList<>();
+        for (Card card : cards.keySet()) {
+            CardButton cardButton = cards.get(card);
+            if (cardButton.isSelected()) {
+                cards.remove(card);
+                removedCards.add(card);
+            }
+        }
+        return removedCards;
     }
 
 }
